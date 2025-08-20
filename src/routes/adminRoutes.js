f0c88fee88d36aa664d4df0a217cf5f2182ad44d
Prakanksh <nagpalprakankshabvpy@gmail.com>
2025-08-20 12:33:59 +0530

    import redefined and tested

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const { loginAdmin, getAdminDashboard, createUser, deleteUser , getUserById , getAllUsers , banUnbanUser ,updateUser , verifyKYC} = require('../controllers/adminController');
+const adminController = require('../controllers/adminController');
 const { protect } = require('../middlewares/authMiddleware');
 const { responseHandler } = require('../utils/responseHandler');
 
 const router = express.Router();
 
-router.post('/login', responseHandler(loginAdmin));
-router.get('/dashboard', protect, responseHandler(getAdminDashboard));
+router.post('/login', responseHandler(adminController.loginAdmin));
+router.get('/dashboard', protect, responseHandler(adminController.getAdminDashboard));
 
 // Admin -> manage users
-router.post("/users", protect, responseHandler(createUser));
-router.delete("/users/:id", protect, responseHandler(deleteUser));
-router.get("/users/:id", protect, responseHandler(getUserById));
-router.get("/users", protect, responseHandler(getAllUsers));
-router.patch("/users/:id/ban", protect, responseHandler(banUnbanUser));
-router.put("/users/:id", protect, responseHandler(updateUser));
-router.patch("/users/:id/kyc", protect, responseHandler(verifyKYC));
+router.post("/users", protect, responseHandler(adminController.createUser));
+router.delete("/users/:id", protect, responseHandler(adminController.deleteUser));
+router.get("/users/:id", protect, responseHandler(adminController.getUserById));
+router.get("/users", protect, responseHandler(adminController.getAllUsers));
+router.patch("/users/:id/ban", protect, responseHandler(adminController.banUnbanUser));
+router.put("/users/:id", protect, responseHandler(adminController.updateUser));
+router.patch("/users/:id/kyc", protect, responseHandler(adminController.verifyKYC));
 
 module.exports = router;
